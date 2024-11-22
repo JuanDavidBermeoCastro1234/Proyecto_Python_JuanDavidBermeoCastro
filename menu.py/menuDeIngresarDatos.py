3c8c89b58017cab726f513c51d881054d018bfc7
JuanDavidBermeoCastro <186082659+JuanDavidBermeoCastro@users.noreply.github.com>
2024-11-22 15:42:06 -0500

    chambeador

---
+++ menu.py/menuDeIngresarDatos.py
@@ -0,0 +1,20 @@
+def Designprincipal ():
+    print(""" 
+          
+    =============================================
+         Simulador de Gasto Diario
+    =============================================
+    Seleccione una opción:
+
+    1. Registrar nuevo gasto
+    2. Listar gast
+    3. Calcular total de gastos
+    4. Generar reporte de gastos
+    5. Salir
+    =============================================
+
+    """)
+    option=int(input("ingrese por favor que opcion desea ejecutar(1-5): "))
+    return option
+
+
---
+++ menu.py/menuDeNuevoGasto.py
@@ -0,0 +1,17 @@
+
+def Nuevogasto():
+    print("""
+    =============================================
+            Registrar Nuevo Gasto
+    =============================================
+    Ingrese la información del gasto:
+
+    - Monto del gasto:
+    - Categoría (ej. comida, transporte, entretenimiento, otros):
+    - Descripción (opcional):
+
+    ingrese 'S' para guardar o 'C' para cancelar.
+    =============================================
+    """)
+    
+    
